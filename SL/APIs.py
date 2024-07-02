33bcc9b6a6df0087dfb702184e8712f833d1e30f
wilkiehoare66 <wilkie@virginland.com>
2024-07-02 11:30:08 +0100

    Create APIs.py

---
+++ SL/APIs.py
@@ -0,0 +1,19 @@
+PD_query_params = {
+	'api_token': 'ffaf91020526e58002f65c7dd5b3f103d411ef73'
+	}
+
+SL_headers = {
+	'Authorization': 'bearer 623d4036-3070-4b1a-9a04-7406bd9e6b28'
+	}
+	
+CR_query_params = {
+	'api_key': '6A9BF842038693B5BCA4'
+	}
+	
+CH_headers = {
+	'Authorization': '61816d6b-1c50-4a20-b2a7-ddf1bba060bf'
+	}
+
+VL_headers = {
+	'x-api-key': 'fx1dE06NkR6qWUbnfoLaU14bdUT7adB665IUd4rv'
+	}
